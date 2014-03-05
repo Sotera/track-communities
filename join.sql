458b5f5b8029f7863fc343cb1952f69d8ff8d482
Justin Gawrilow <justin.gawrilow@gmail.com>
2014-03-05 01:59:57 +0000

    unneeded files

--- join.sql
+++
@@ -1,4 +0,0 @@
-drop table ${hiveconf:table}_tracks_comms_joined;
-create table ${hiveconf:table}_tracks_comms_joined as 
-select a.intersectx, a.intersecty, a.dt, a.track_id, b.comm
-from micro_path_tripline_bins_${hiveconf:table} a join nodes_comms b on (a.track_id = b.node);
