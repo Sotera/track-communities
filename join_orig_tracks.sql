7ba83b004024beee6407e13586778f4566826362
George Zheng <gzheng@staging1.eagc.dev>
2018-06-01 12:02:40 -0700

    added database argument

--- join_orig_tracks.sql
+++ join_orig_tracks.sql
@@ -1,3 +1,4 @@
+use ${hiveconf:database};
 drop table ${hiveconf:table}_tracks_comms_joined;
 create table ${hiveconf:table}_tracks_comms_joined as
 select a.${hiveconf:latitude} as intersectx, a.${hiveconf:longitude} as intersecty, a.${hiveconf:dt} as dt, a.${hiveconf:id} as track_id, b.*
